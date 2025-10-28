58d095ac1f2c014822d8261e24d161cf072e2673
arisetia04 <ari750142@gmail.com>
2025-10-28 11:02:50 +0700

    [feat] Add request validator

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.0
 toolchain go1.24.4
 
 require (
+	github.com/go-playground/validator/v10 v10.28.0
 	github.com/google/uuid v1.6.0
 	github.com/labstack/echo/v4 v4.13.4
 	github.com/rabbitmq/amqp091-go v1.10.0
@@ -15,6 +16,9 @@ require (
 )
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.6 // indirect
@@ -22,6 +26,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
---
+++ validator.go
@@ -0,0 +1,51 @@
+package sw
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+)
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		if validationErrors, ok := err.(validator.ValidationErrors); ok {
+			var messages []string
+			for _, e := range validationErrors {
+				switch e.Tag() {
+				case "required":
+					messages = append(messages, fmt.Sprintf("%s is required", toSnakeCase(e.Field())))
+				case "e164":
+					messages = append(messages, fmt.Sprintf("%s must be a valid phone number (e.g. +628123456789)", toSnakeCase(e.Field())))
+				case "numeric":
+					messages = append(messages, fmt.Sprintf("%s must be numeric", toSnakeCase(e.Field())))
+				case "oneof":
+					messages = append(messages, fmt.Sprintf("%s must be one of [%s]", toSnakeCase(e.Field()), e.Param()))
+				default:
+					messages = append(messages, fmt.Sprintf("%s is invalid", toSnakeCase(e.Field())))
+				}
+			}
+			return echo.NewHTTPError(400, strings.Join(messages, ", "))
+		}
+		return err
+	}
+	return nil
+}
+
+// toSnakeCase turns "PhoneNumber" → "phone_number"
+func toSnakeCase(str string) string {
+	var result []rune
+	for i, r := range str {
+		if i > 0 && r >= 'A' && r <= 'Z' {
+			result = append(result, '_', r+('a'-'A'))
+		} else {
+			result = append(result, r)
+		}
+	}
+	return strings.ToLower(string(result))
+}
